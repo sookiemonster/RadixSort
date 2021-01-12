16b660e1c730a8ec977a362a7cd1296f67fb8e24
sookiemonster <dsooknanan20@stuy.edu>
2021-01-12 12:51:32 -0500

    Added merge() and tests. Added more tests for length.

--- Driver.java
+++ Driver.java
@@ -1,16 +1,57 @@
 public class Driver {
   public static void main(String[] args) {
+    System.out.println("Nth Tests");
     System.out.println(Radix.nth(123,1));
     System.out.println(Radix.nth(-123,1));
     System.out.println(Radix.nth(123,2));
     System.out.println(Radix.nth(-123,2));
+
     System.out.println();
-    System.out.println(Radix.length(0));
-    System.out.println(Radix.length(15));
-    System.out.println(Radix.length(-10));
-    System.out.println(Radix.length(5112));
-    System.out.println(Radix.length(51122));
-    System.out.println(Radix.length(5));
-    System.out.println(Radix.length(-5));
+    System.out.println("Length Tests");
+    lengthTest(10, 10000);
+    System.out.println();
+    System.out.println("Merge Tests");
+    mergeTest(4, 100);
+  }
+
+  public static String randomNumber(int maxNum) {
+    String randNum = "" + (int)(Math.random()*maxNum);
+    return randNum;
+  }
+
+  public static void lengthTest(int tests, int maxNum) {
+    for (int i = 0; i < tests; i++) {
+      String temp = randomNumber(maxNum);
+      System.out.println(temp + ": " + Radix.length(Integer.parseInt(temp)));
+    }
+
+    for (int i = 0; i < tests; i++) {
+      String temp = "" + (-1 * Integer.parseInt(randomNumber(maxNum)));
+      System.out.println(temp + ": " + Radix.length(Integer.parseInt(temp)));
+    }
+  }
+
+  public static MyLinkedList randomList(int size, int maxNum) {
+    MyLinkedList result = new MyLinkedList();
+    for (int i = 0; i < size; i++) {
+      result.add(randomNumber(maxNum));
+    }
+    return result;
+  }
+
+  public static void mergeTest(int size, int maxLength) {
+    MyLinkedList a = randomList(size, maxLength);
+    MyLinkedList[] arr = new MyLinkedList[4];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = (randomList(size, maxLength));
+    }
+    System.out.println("Original: ");
+    System.out.println(a);
+    System.out.println("Buckets: ");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    Radix.merge(a, arr);
+    System.out.println(a);
   }
 }
--- Radix.java
+++ Radix.java
@@ -12,7 +12,9 @@ public class Radix {
     return (int)Math.floor(Math.log10(n)) + 1;
   }
 
-  // public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
-  //
-  // }
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+    for (int i = 0; i < buckets.length; i++) {
+      original.extend(buckets[i]);
+    }
+  }
 }
